b5a4e0f3174716d312b1b4f210c3f1559e65affb
brunorwanda4 <brunorwanda4@gmail.com>
2025-05-18 15:06:12 +0200

    finish to make member

--- frontend/src/pages/members.jsx
+++ frontend/src/pages/members.jsx
@@ -1,70 +1,76 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BsPeople, BsPeopleFill, BsPlus } from "react-icons/bs";
+import { BsPeopleFill, BsPlus } from "react-icons/bs";
 import DeleteMember from "../components/delete-member";
 import { NavLink } from "react-router-dom";
 
 const Members = () => {
   const [members, setMembers] = useState([]);
   const [error, setError] = useState("");
+
   useEffect(() => {
+    let intervalId;
+
     const fetchData = async () => {
       try {
-        const req = await axios.get("http://localhost:3012/members");
-        if (req.data) {
-          setMembers(req.data);
+        const res = await axios.get("http://localhost:3012/members");
+        if (res.data) {
+          setMembers(res.data);
+          setError("");
         }
       } catch (error) {
         const err = error.response?.data?.message || "Server error!";
         setError(err);
       }
     };
-
     fetchData();
+    intervalId = setInterval(fetchData, 1000);
+    return () => clearInterval(intervalId);
   }, []);
+
   return (
-    <div className=" c">
-      <div className=" flex justify-between items-center mb-10">
-        <h1 className=" flex space-x-2 items-center font-bold text-3xl">
+    <div className="c">
+      <div className="flex justify-between items-center mb-10">
+        <h1 className="flex space-x-2 items-center font-bold text-3xl">
           <BsPeopleFill /> <span>Members</span>
         </h1>
-        <NavLink to={"/admin/members/add"} className=" btn btn-secondary">
+        <NavLink to={"/admin/members/add"} className="btn btn-secondary">
           <BsPlus /> Add Members
         </NavLink>
       </div>
-      {error && <div className=" alert alert-error"> 😔 {error} </div>}
-      <table className=" table table-zebra">
-        <tHead>
+
+      {error && <div className="alert alert-error">😔 {error}</div>}
+
+      <table className="table table-zebra">
+        <thead>
           <tr>
-            <td>Id</td>
-            <td>First Name</td>
-            <td>Last Name</td>
-            <td>email</td>
-            <td>Phone</td>
-            <td>Action</td>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Action</th>
           </tr>
-        </tHead>
+        </thead>
         <tbody>
-          {members.map((item) => {
-            return (
-              <tr>
-                <td>{item.memberId}</td>
-                <td>{item.firstName}</td>
-                <td>{item.lastName}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-                <td className=" flex flex-row gap-2 space-x-2">
-                  <DeleteMember member={item} />
-                  <NavLink
-                    to={`/admin/members/edit/${item.memberId}`}
-                    className="  btn btn-sm btn-warning"
-                  >
-                    Edit
-                  </NavLink>
-                </td>
-              </tr>
-            );
-          })}
+          {members.map((item) => (
+            <tr key={item.memberId}>
+              <td>{item.memberId}</td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td className="flex flex-row gap-2">
+                <DeleteMember member={item} />
+                <NavLink
+                  to={`/admin/members/edit/${item.memberId}`}
+                  className="btn btn-sm btn-warning"
+                >
+                  Edit
+                </NavLink>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
